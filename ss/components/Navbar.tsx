b541f6f2df65544099708e83aa9cd4a0bb937bf6
Natnael Endale <n87294578@gmail.com>
2026-05-15 23:36:20 +0300

    add the signout btn

--- ss/components/Navbar.tsx
+++ ss/components/Navbar.tsx
@@ -1,9 +1,14 @@
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
 
 import Logo from "../public/logo.png";
+import { signOut, useSession } from "next-auth/react";
 
 function Navbar() {
+  const { data: session } = useSession();
+
   return (
     <nav className="bg-white shadow">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -31,24 +36,36 @@ function Navbar() {
               >
                 Browse Jobs
               </Link>
-              <Link
-                href="/jobs/posts"
-                className="text-gray-800 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-              >
-                Post a Job
-              </Link>
-              <Link
-                href="/dashboard"
-                className="text-gray-800 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-              >
-                Dashboard
-              </Link>
-              <Link
-                href="/auth/signin"
-                className="text-gray-800 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-              >
-                Sign In
-              </Link>
+              {session ? (
+                <>
+                  <Link
+                    href="/jobs/posts"
+                    className="text-gray-800 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  >
+                    Post a Job
+                  </Link>
+
+                  <Link
+                    href="/dashboard"
+                    className="text-gray-800 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  >
+                    Dashboard
+                  </Link>
+                  <button
+                    onClick={() => signOut()}
+                    className="text-gray-800 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  >
+                    Sign Out
+                  </button>
+                </>
+              ) : (
+                <Link
+                  href="/auth/signin"
+                  className="text-gray-800 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Sign In
+                </Link>
+              )}
             </div>
           </div>
         </div>
