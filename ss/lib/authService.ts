37adf49f3fb4685b60ec04cf10bfe49c425299c7
Natnael Endale <n87294578@gmail.com>
2026-05-15 22:09:10 +0300

    add the login and logout

---
+++ ss/lib/authService.ts
@@ -0,0 +1,13 @@
+import { signIn, signOut } from "@/auth";
+
+export async function logIn() {
+  await signIn("github", {
+    redirectTo: "/",
+  });
+}
+
+export async function logOut() {
+  await signOut({
+    redirectTo: "/",
+  });
+}
